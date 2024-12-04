68f87ad9aa997ba3ca543e337a1ddd53a4155a9f
fenghao <fh@fhcy.vip>
2024-12-04 15:14:39 +0800

    创建项目

---
+++ src/views/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-header class="header">
+      </el-header>
+      <el-main class="main">
+      </el-main>
+      <el-footer>
+
+      </el-footer>
+    </el-container>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+.header {
+  padding: 0;
+  display: flex;
+  justify-content: center; /* 水平居中 */
+  align-items: center;     /* 垂直居中 */
+  background-color: #011f3c;
+}
+.main {
+}
+</style>
